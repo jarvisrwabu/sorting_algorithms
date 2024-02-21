419bfca3f2a3a46c96841967c75491b7183e7654
Jarvis Rwabuyongo <jarvisrwabuyongo45@gmail.com>
2024-02-21 13:42:41 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,28 +16,6 @@ void swap(int *a, int *b)
 }
 
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-	size_t i;
-
-	i = 0;
-	while (array && i < size)
-	{
-		if (i > 0)
-		printf(", ");
-		printf("%d", array[i]);
-		++i;
-	}
-	printf("\n");
-}
-
-
-/**
  * bubble_sort - Arrange in ascending order using
  * bubble sort algorithm
  * @array: Pointer to array
